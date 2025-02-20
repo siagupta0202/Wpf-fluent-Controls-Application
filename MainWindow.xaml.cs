85a2e6d087f1908ff53a1cf449f9c7da90fc6392
Sia Gupta <siagupta@microsoft.com>
2025-02-20 18:52:05 +0530

    Adding Listview for the controls

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Configuration;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfFluentControls.ControlPages;
 
 namespace WpfFluentControls;
 
@@ -19,5 +21,29 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        MainFrame.Navigate(new GridSplitterPage()); // Default page
+    }
+
+    private void NavigationList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (NavigationList.SelectedItem is ListViewItem selectedItem)
+        {
+            string pageName = selectedItem.Tag.ToString();
+            switch (pageName)
+            {
+                case "GridSplitter.xaml":
+                    MainFrame.Navigate(new GridSplitterPage());
+                    break;
+                case "GroupBoxPage.xaml":
+                    MainFrame.Navigate(new GroupBoxPage());
+                    break;
+                case "ThumbPage.xaml":
+                    MainFrame.Navigate(new ThumbPage());
+                    break;
+                case "HyperlinkPage.xaml":
+                    MainFrame.Navigate(new HyperlinkPage());
+                    break;
+            }
+        }
     }
 }
